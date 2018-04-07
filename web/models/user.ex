cedd3a5e46bd0f0d6e84587a348381ab4f9acb65
Kevin Pfefferle <kevin@pfefferle.co>
2018-04-07 00:59:13 -0400

    Display User create errors

--- web/models/user.ex
+++ web/models/user.ex
@@ -13,6 +13,7 @@ defmodule Rumbl.User do
   def changeset(model, params \\ :invalid) do
     model
     |> cast(params, ~w(name username), [])
-    |> validate_length(:username, min: 1, max: 20)
+    |> validate_required([:name, :username, :password])
+    |> validate_length(:username, min: 2, max: 20)
   end
 end
